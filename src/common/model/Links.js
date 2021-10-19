bb4fa52701f4ada75271e08e4560f2d4f41fe1d1
Martin <1808402272@qq.com>
2021-10-19 16:23:32 +0800

    优化：丰富链接选择框里的内容

--- src/common/model/Links.js
+++ src/common/model/Links.js
@@ -38,6 +38,14 @@ const basics = {
       param: {
         path: 'pages/user/index'
       }
+    },
+    {
+      id: '593fe1f',
+      title: '会员登录页',
+      type: LINK_TYPE_PAGE,
+      param: {
+        path: 'pages/login/index'
+      }
     }
   ]
 }
@@ -95,63 +103,12 @@ const store = {
       }
     },
     {
-      id: '91th4ss',
-      title: '自定义页',
-      type: LINK_TYPE_PAGE,
-      param: {
-        path: 'pages/custom/index',
-        query: {}
-      },
-      form: [
-        {
-          key: 'query.pageId',
-          lable: '页面ID',
-          required: true,
-          tips: '店铺管理 -> 店铺页面'
-        }
-      ]
-    },
-    {
       id: '56sswhq',
       title: '领券中心',
       type: LINK_TYPE_PAGE,
       param: {
         path: 'pages/coupon/index'
       }
-    },
-    {
-      id: 'ugrauzv',
-      title: '文章列表页',
-      type: LINK_TYPE_PAGE,
-      param: {
-        path: 'pages/article/index',
-        query: {}
-      },
-      form: [
-        {
-          key: 'query.categoryId',
-          lable: '分类ID',
-          tips: '内容管理 -> 文章分类',
-          value: ''
-        }
-      ]
-    },
-    {
-      id: 'u1v6aux',
-      title: '文章详情页',
-      type: LINK_TYPE_PAGE,
-      param: {
-        path: 'pages/article/detail',
-        query: {}
-      },
-      form: [
-        {
-          key: 'query.articleId',
-          lable: '文章ID',
-          required: true,
-          tips: '内容管理 -> 文章列表'
-        }
-      ]
     }
   ]
 }
@@ -180,11 +137,35 @@ const personal = {
       ]
     },
     {
+      id: 'c4f630d',
+      title: '我的钱包页',
+      type: LINK_TYPE_PAGE,
+      param: {
+        path: 'pages/wallet/index'
+      }
+    },
+    {
+      id: '792db19',
+      title: '充值中心页',
+      type: LINK_TYPE_PAGE,
+      param: {
+        path: 'pages/wallet/recharge/index'
+      }
+    },
+    {
       id: '03b9290',
       title: '我的优惠券',
       type: LINK_TYPE_PAGE,
       param: {
         path: 'pages/my-coupon/index'
+      }
+    },
+    {
+      id: '569b0b0',
+      title: '会员积分明细',
+      type: LINK_TYPE_PAGE,
+      param: {
+        path: 'pages/points/log'
       }
     },
     {
@@ -206,4 +187,80 @@ const personal = {
   ]
 }
 
-export const linkList = [basics, store, personal]
+// 其他页面
+const other = {
+  title: '其他页面',
+  key: 'other',
+  data: [
+    {
+      id: '91th4ss',
+      title: '自定义页',
+      type: LINK_TYPE_PAGE,
+      param: {
+        path: 'pages/custom/index',
+        query: {}
+      },
+      form: [
+        {
+          key: 'query.pageId',
+          lable: '页面ID',
+          required: true,
+          tips: '店铺管理 -> 店铺页面'
+        }
+      ]
+    },
+    {
+      id: 'ugrauzv',
+      title: '资讯列表页',
+      type: LINK_TYPE_PAGE,
+      param: {
+        path: 'pages/article/index',
+        query: {}
+      },
+      form: [
+        {
+          key: 'query.categoryId',
+          lable: '分类ID',
+          tips: '内容管理 -> 文章分类',
+          value: ''
+        }
+      ]
+    },
+    {
+      id: 'u1v6aux',
+      title: '资讯详情页',
+      type: LINK_TYPE_PAGE,
+      param: {
+        path: 'pages/article/detail',
+        query: {}
+      },
+      form: [
+        {
+          key: 'query.articleId',
+          lable: '文章ID',
+          required: true,
+          tips: '内容管理 -> 文章列表'
+        }
+      ]
+    }
+  ]
+}
+
+// // 门店 - 拨打电话 - 暂未实现
+// const callPhone = {
+//   id: 'cb344ba',
+//   title: '拨打电话',
+//   type: 'callPhone',
+//   param: {
+//     phoneNumber: '13212341234'
+//   },
+//   form: [
+//     {
+//       key: 'phoneNumber',
+//       lable: '电话号码',
+//       tips: ''
+//     }
+//   ]
+// }
+
+export const linkList = [basics, store, personal, other]
