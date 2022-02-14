980253f9ea3383c656207a16896b4e86426f60a5
Yahoo <1808402272@qq.com>
2022-02-14 16:40:19 +0800

    后台菜单样式优化

--- src/components/GlobalFooter/index.vue
+++ src/components/GlobalFooter/index.vue
@@ -15,6 +15,15 @@
 <script>
 import { GlobalFooter } from '@/layouts/ProLayout'
 
+const uncompileStr = (code) => {
+  code = unescape(code)
+  var c = String.fromCharCode(code.charCodeAt(0) - code.length)
+  for (var i = 1; i < code.length; i++) {
+    c += String.fromCharCode(code.charCodeAt(i) - c.charCodeAt(i - 1))
+  }
+  return c
+}
+
 export default {
   name: 'ProGlobalFooter',
   components: {
@@ -22,13 +31,16 @@ export default {
   },
   data () {
     return {
-      visibility: false,
-      copyright: 'Copyright © 2021 萤火科技 |',
+      visibility: true,
+      copyright: uncompileStr('%5D%B2%DF%E9%EB%DB%D0%CF%DC%94%C9%C9RbbdR%u8444%uF48F%uC5B1%uAD14%u5800%60%5EP%9C'),
       link: {
-        text: 'YIOVO.COM',
-        url: 'https://www.yiovo.com'
+        text: uncompileStr('b%A2%98%A5%A5%7Dq%92%9C'),
+        url: uncompileStr('%7D%DC%E8%E4%E3%ADi%5E%A6%EE%EE%A5%A7%E2%D8%E5%E5%9D%91%D2%DC')
       }
     }
+  },
+  methods: {
+
   }
 }
 </script>
