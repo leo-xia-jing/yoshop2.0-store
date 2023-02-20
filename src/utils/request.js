ad0c753130ab0b4d9b6431104af41ddd140b9bf5
萤火科技 <635993292@qq.com>
2023-02-20 14:00:56 +0800

    优化；调整上传视频文件大小限制为20m

--- src/utils/request.js
+++ src/utils/request.js
@@ -16,7 +16,7 @@ const service = axios.create({
   // 服务端api地址
   // baseURL: process.env.NODE_ENV === 'production' ? publicConfig.BASE_API : process.env.VUE_APP_API_BASE_URL,
   baseURL: publicConfig.BASE_API,
-  timeout: 25 * 1000 // 请求超时时间，25秒, 如果上传大文件需要更长
+  timeout: 60 * 1000 // 请求超时时间，60秒, 如果上传大文件需要更长
 })
 
 // 接口请求拦截
