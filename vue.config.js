ac06be251e41dd1222f928567a03d4db3fb178fd
萤火科技 <635993292@qq.com>
2023-02-20 13:02:01 +0800

    修改端口号

--- vue.config.js
+++ vue.config.js
@@ -102,7 +102,7 @@ const vueConfig = {
 
   devServer: {
     // development server port 8000
-    port: 8210
+    port: 8120
     // If you want to turn on the proxy, please remove the mockjs /src/main.jsL11
     // proxy: {
     //   '/api': {
